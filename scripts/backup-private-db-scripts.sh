e33916ef8b80a9579859f01587911ebfbe66f056
60591511 <605391511@qq.com>
2026-07-07 12:04:19 +0800

    feat: add private_db script backup to dotfiles workflow

---
+++ scripts/backup-private-db-scripts.sh
@@ -0,0 +1,5 @@
+# 将 private_db 工具脚本纳入 dotfiles 备份
+cp /home/bobobears/private_db/db.py /home/bobobears/dotfiles/private_db/
+cp /home/bobobears/private_db/init_schema.py /home/bobobears/dotfiles/private_db/
+cp /home/bobobears/private_db/is_trading_day.py /home/bobobears/dotfiles/private_db/
+cp /home/bobobears/private_db/save_dsa_scores.py /home/bobobears/dotfiles/private_db/
